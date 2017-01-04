9d73bd5309089e905490eff7bcf51c3f6dc8fb55
satbirdd <satbirdd@gmail.com>
2017-01-04 21:14:36 +0800

    day can be created only whin 4 days

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,8 +3,17 @@ class Day < ApplicationRecord
   belongs_to :theme
 
   validates :date, uniqueness: true, presence: true
+  validate :date_validate
 
   scope :recent_year, -> {
     where("created_at >= :last_year", last_year: Date.today.last_year)
   }
+
+  private
+
+  def date_validate
+    if date > Date.today || date < Date.today.days_ago(3)
+      errors.add(:base, "无效的日期，只能是最近四天之内！")
+    end
+  end
 end
