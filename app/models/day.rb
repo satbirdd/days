855926c5a0a5e3e93252a0c89470f5f24d080de8
vagrant <vagrant@precise32.(none)>
2017-01-04 13:09:01 +0000

    day validate date

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,6 +2,8 @@ class Day < ApplicationRecord
   belongs_to :user
   belongs_to :theme
 
+  validates :date, uniqueness: true, presence: true
+
   scope :recent_year, -> {
     where("created_at >= :last_year", last_year: Date.today.last_year)
   }
